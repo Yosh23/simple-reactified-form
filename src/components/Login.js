dc5fded4b9c55082b041c6286dfde1fbf0f14ef6
Josh Riches <joshriches@gmail.com>
2019-06-20 19:53:21 -0600

    setState in Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 export default class extends React.Component {
+    state = {
+        name: 'josh'
+    }
     render () {
+        console.log(this.state.name)
         return (
             <>
                 <h1> Login Page </h1>
                 <form>
                     <p>User Name: </p>
-                    <input type="text" placeholder="Name Please..." required />
+                    <input type="text" placeholder="Name Please..." onChange={(event) => this.setState({name: event.target.value})}  required />
                 </form>
             </>
         )
